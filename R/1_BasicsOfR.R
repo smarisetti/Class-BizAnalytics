a741ebde15f539f1c607b8aa85465162d0191140
Sudheer Marisetti <sudheer@abacus-concepts.com>
2014-08-07 15:43:43 +0530

    New R scripts for Intro class

---
+++ R/1_BasicsOfR.R
@@ -0,0 +1,52 @@
+# Basic calculation
+8 * 6
+2**16
+
+ # Functions
+sqrt(2)
+abs(-25)
+?sqrt
+
+# Variables
+SquareRoot2 = sqrt(2)
+SquareRoot2
+HoursYears <- 365 * 24
+HoursYears
+# variables are case sensitive
+ls()
+rm()
+ls()
+
+# Vector - series of numbers stored as same object
+
+c(2,3,5,8,13)
+# c - create a vector
+Country = c("Brazil","China","India","Switzerland","USA")
+# Life expentancy in these countries
+LifeExpectancy = c(74,76,65,83,79)
+Country
+LifeExpectancy
+c("Brazil",74,"China",76)
+# look at the output. All are converted to text.
+Country[1]
+LifeExpectancy[3]
+# Sequence of numbers
+Sequence = seq(1,100,2)
+Sequence
+
+Data = data.frame(Country, LifeExpectancy)
+Data
+# Data frame maintains the data types
+# New vector Population
+Population = c(199000, 1390000, 1240000, 7997, 318000)
+Data2 = cbind(Data, Population)
+Data2
+# cbind - column binding)
+# Row binding
+Country = c("Australia", "Greece")
+LifeExpectancy = c(82,81)
+Population = c(23050, 11125)
+NewData = data.frame(Country,LifeExpectancy,Population)
+NewData
+Data3 = rbind(Data2, NewData)
+Data3
---
+++ R/2_LoadingData.R
@@ -0,0 +1,54 @@
+# We will use WHO.csv data file in this
+
+# Navigate to the location
+# CWD - change working director to location where the file is
+setwd("~/Box/Professional/ClassesTaken/edX/AnalyticsEdge/1_Intro")
+getwd()
+WHO = read.csv("WHO.csv")
+str(WHO)
+summary(WHO)
+WHO_Europe = subset(WHO, Region == "Europe")
+str(WHO_Europe)
+write.csv(WHO_Europe, "WHO_Europe.csv")
+
+ls()
+rm(WHO_Europe)
+ls()
+
+# Data Analysis
+Under15
+WHO$Under15
+# Mean
+mean(WHO$Under15)
+# standard deviation
+sd(WHO$Under15)
+# Statistical Summary
+summary(WHO$Under15)
+which.min(WHO$Under15)
+# it gives the row # in the data frame
+WHO$Country[86]
+which.max(WHO$Under15)
+WHO$Country[124]
+
+# Plot creation
+# GNI and Fertility
+plot(WHO$GNI, WHO$FertilityRate)
+Outliers = subset(WHO, GNI > 10000 & FertilityRate > 2.5)
+nrow(Outliers) # number of rows
+Outliers[c("Country", "GNI", "FertilityRate")]
+
+# Plots: Histogram
+hist(WHO$CellularSubscribers)
+boxplot(WHO$LifeExpectancy ~ WHO$Region)
+boxplot(WHO$LifeExpectancy ~ WHO$Region, xlab = "Region", ylab ="Life Expectancy", main = "Life Expectancy of Countries by Region")
+
+# Summary Tables
+table(WHO$Region)
+tapply(WHO$Over60, WHO$Region, mean) # GROUP BY function
+tapply(WHO$LiteracyRate, WHO$Region, min)
+# shows some missing values
+# How do we remove the missing values?
+tapply(WHO$LiteracyRate, WHO$Region, min, na.rm=TRUE)
+# You see no more NA values
+
+# Saving file
